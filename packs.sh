d38f72241096bbf9b6febef670e62f8813fd3565
bluboy-official <56422076+bluboy-official@users.noreply.github.com>
2021-01-24 17:23:27 +0800

    Delete packs.sh

--- packs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cp sources.list /etc/apt
-apt update
-apt install -y chromium-browser
-apt install -y chromium-driver
-apt install -y python3-pip
-pip3 install selenium requests schedule
